c5e6fc9ee8570ab5397d08d46bf04319ee1b745b
Abdul Khan <ahkn63@gmail.com>
2026-08-25 14:28:40 -0400

    🎉 chore(Apple Homepage): Initial Commit & Navbar Section

---
+++ App Valley/Apple Homepage/src/constants/index.ts
@@ -0,0 +1,10 @@
+export const navLinks = [
+  { label: "Store" },
+  { label: "Mac" },
+  { label: "iPad" },
+  { label: "iPhone" },
+  { label: "Watch" },
+  { label: "Vision" },
+  { label: "AirPods" },
+  { label: "TV & Home" },
+]
